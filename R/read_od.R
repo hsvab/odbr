425d38a8b6171a74d6a8459a22e525be38ba7d44
Haydée Svab <hsvab@hsvab.eng.br>
2023-06-03 11:20:15 -0300

    With error message if the city is not avaiable

--- R/read_od.R
+++ R/read_od.R
@@ -2,19 +2,28 @@
 # read_od (city = , year = , harmonize = TRUE/FALSE)
 # harmonize vai filtrar se vamos pegar as bases compatibilizadas ou não
 
-read_od <- function(city, year, harmonize = FALSE) {
+read_od <- function(city = "São Paulo", year = 1977, harmonize = FALSE) {
 
 # checar argumentos: se passar cidade inexistente, dá mensagem de erro
+if(!city %in% metadata$city){
+  usethis::ui_stop("The specified city ({city}) is not available.
+                   Check the metadata object for available cities and cohorts.")
+}
 
 # checar argumentos: se passar cidade + ano inexistente, dá mensagem de erro
 
-# checar argumentos: se passar cidade + ano inexistente, dá mensagem de erro
+
+# checar argumentos: se passar cidade + ano + parâmetro de hamonized inexistente
+# dá mensagem de erro
+
 
 # Crio um caminho de um arquivo temporário e salvo no objeto temp do R
 temp <- fs::file_temp()
 
 # Crio o nome do arquivo que quero fazer download
 filename_to_download <- "od_sao-paulo_1977_not-harmonized.csv.gz"
+
+# TODO: compor o nome do arquivo que vamos baixar
 
 # Crio efetivamnete a pasta temporária
 fs::dir_create(temp)
