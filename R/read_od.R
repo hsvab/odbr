b63d4d4c538b0ad046e5ee8a1bbfafd1759cb8b7
Haydée Svab <hsvab@hsvab.eng.br>
2023-06-03 11:00:21 -0300

    Download in a temp folder and read function structure (with return)

--- R/read_od.R
+++ R/read_od.R
@@ -1,4 +1,7 @@
 
+# read_od (city = , year = , harmonize = TRUE/FALSE)
+# harmonize vai filtrar se vamos pegar as bases compatibilizadas ou não
+
 read_od <- function(city, year, harmonize = FALSE) {
 
 # checar argumentos: se passar cidade inexistente, dá mensagem de erro
@@ -7,12 +10,29 @@ read_od <- function(city, year, harmonize = FALSE) {
 
 # checar argumentos: se passar cidade + ano inexistente, dá mensagem de erro
 
+# Crio um caminho de um arquivo temporário e salvo no objeto temp do R
 temp <- fs::file_temp()
 
+# Crio o nome do arquivo que quero fazer download
+filename_to_download <- "od_sao-paulo_1977_not-harmonized.csv.gz"
+
+# Crio efetivamnete a pasta temporária
+fs::dir_create(temp)
+
+# Crio o caminho + nome de arquivo
+temporary_filename <- paste0(temp, "/", filename_to_download)
+
 # Subindo o arquivo para um release do repo odbr, release especificado no parâmetro
-piggyback::pb_download(file = "od_sao-paulo_1977_not-harmonized.csv.gz",
+piggyback::pb_download(file = filename_to_download,
                          repo = "hsvab/odbr",
                          tag = "v0.0.1",
                          dest = temp)
+
+# Subindo o arquivo para um release do repo odbr, release especificado no parâmetro
+od_file <- readr::read_csv2(temporary_filename)
+
+fs::dir_delete(temp)
+
+od_file
 
 }
