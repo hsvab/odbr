9d8197d5728aae659b47a9de6a7a5d8928556f59
Haydée Svab <hsvab@hsvab.eng.br>
2023-06-04 16:57:53 -0300

    Translating R code comments to english - read_od.R file

--- R/read_od.R
+++ R/read_od.R
@@ -1,35 +1,35 @@
-
-# read_od (city = , year = , harmonize = TRUE/FALSE)
-# harmonize vai filtrar se vamos pegar as bases compatibilizadas ou não
-
+# The "read_od" function requires as parameter city, year and whether you want
+# the harmonised database (over the years, for the same city) or not - the default is the raw base
 read_od <- function(city = "São Paulo", year = 1977, harmonize = FALSE) {
 
-# checar argumentos: se passar cidade inexistente, dá mensagem de erro
+# Argument check - if it is passed a non-existent city in odbr, it gives an error message
 if(!city %in% metadata$city){
   usethis::ui_stop("The specified city ({city}) is not available.
                    Check the metadata object for available cities and cohorts.")
 }
 
-# checar argumentos: se passar cidade + ano inexistente, dá mensagem de erro
+# Argument check - if it is passed a valid city but with a non-existent year in
+# odbr, it gives an error message
 
+# Argument check - if it is passed a valid city with a valid year, but with a
+# non-existent harmonized parameter, it gives an error message
 
-# checar argumentos: se passar cidade + ano + parâmetro de hamonized inexistente
-# dá mensagem de erro
+# Creating the filename to download
+filename_to_download <- "od_sao-paulo_1977_not-harmonized.csv.gz"
 
 # TODO: compor o nome do arquivo que vamos baixar
 
-# Crio o nome do arquivo que quero fazer download
-filename_to_download <- "od_sao-paulo_1977_not-harmonized.csv.gz"
-
-# Chamo a função download passando filename_to_download como parâmetro e salvo o
-# retorno na variável temporary_filename
+# Calling the "download_piggyback" function with "filename_to_download" as
+# parameter and saving the function return in "temporary_filename"
 temporary_filename <- download_piggyback(filename_to_download)
 
-# Subindo o arquivo para um release do repo odbr, release especificado no parâmetro
+# Uploading the file to a release in odbr repository
 od_file <- readr::read_csv2(temporary_filename)
 
+# Deleting the "temporary_filename" variable
 fs::file_delete(temporary_filename)
 
+# Delivering the requested file as a function return
 return(od_file)
 
 }
