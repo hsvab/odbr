03d6ad87a4dca1f261d0609d0223a955700bcfed
Haydée Svab <hsvab@hsvab.eng.br>
2023-08-26 11:23:02 -0300

    Carregando o dicionário de dados SP / 1977 / não harmonizado

--- data-raw/sao_paulo/prep_data-sao_paulo.R
+++ data-raw/sao_paulo/prep_data-sao_paulo.R
@@ -1,12 +1,12 @@
 # year 1977 ---------------------------------------------------------------
 
 # lendo arquivo bruto
-od_sp_1977_not_harmonized <- readr::read_csv2("data-raw/sao_paulo/1977/not_harmonized/OD_1977_processado.csv")
+od_sp_1977_not_harmonized <- read_sav("data-raw/sao_paulo/1977/not_harmonized/od_sao-paulo_1977_not-harmonized.sav")
 
 # salvando arquivo em formato compactado (.gz)
 readr::write_csv2(od_sp_1977_not_harmonized, "data-raw/sao_paulo/1977/not_harmonized/od_sao-paulo_1977_not-harmonized.csv.gz")
 
-# criando uma release nova no repositório
+# Criar uma release nova no repositório
 piggyback::pb_new_release(repo = "hsvab/odbr",
                           tag = "v0.0.1")
 
@@ -15,3 +15,8 @@ piggyback::pb_upload(file = "data-raw/sao_paulo/1977/not_harmonized/od_sao-paulo
                      repo = "hsvab/odbr",
                      tag = "v0.0.1")
 
+# Disponibilizar o dicionário de dados
+od_sp_1977_not_harmonized_dictionary <- readr::read_csv2("data-raw/sao_paulo/1977/not_harmonized/od_sao-paulo_1977_not-harmonized-dicionary.csv")
+
+#
+usethis::use_data(od_sp_1977_not_harmonized_dictionary)
