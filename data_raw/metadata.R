bb75fb4c46dfab5009e9fd237829686ec2667ee8
Haydée Svab <hsvab@hsvab.eng.br>
2023-08-31 15:13:57 -0300

    Some updates on metadata script and csv - including language parameter

---
+++ data_raw/metadata.R
@@ -0,0 +1,6 @@
+## code to prepare `metadata` dataset goes here
+
+metadata <- readr::read_csv("data_raw/metadata.csv")
+
+# Making the metadata.csv available to users of the package as database
+usethis::use_data(metadata, overwrite = TRUE)
