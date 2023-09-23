0f67fa8b689d48edd3db5f9f197e6d25840c5ae0
Beatriz Milz <milz.bea@gmail.com>
2023-09-23 17:41:19 -0300

    Styler: utils.r

--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,11 @@
 # Function to compose the base name of data / dictionary files
-compose_name <- function(city, year, harmonize){
+compose_name <- function(city, year, harmonize) {
   city_text <- gsub("~", "", gsub(" ", "-", tolower(iconv(city, to = "ASCII//TRANSLIT"))))
 
   harmonized_text <- "not-harmonized"
-  if(harmonize == TRUE){
+  if (harmonize == TRUE) {
     harmonized_text <- "harmonized"
   }
-  name <- paste0("od_",city_text,"_", year,"_", harmonized_text)
+  name <- paste0("od_", city_text, "_", year, "_", harmonized_text)
   return(name)
 }
