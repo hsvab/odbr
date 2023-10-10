86a28501a8d84a3ec794f82e1fa8301433ea2966
Beatriz Milz <milz.bea@gmail.com>
2023-10-10 10:08:11 -0300

    more similar tests to improve covr

--- tests/testthat/test-read_map.R
+++ tests/testthat/test-read_map.R
@@ -9,4 +9,33 @@ test_that("read_map() works", {
   expect_equal(is.data.frame(testing_object), TRUE)
 
   expect_s3_class(testing_object, "sf")
+
+  # Testing error message
+  expect_snapshot(
+    read_map(
+      city = "Manaus",
+      year = 1977,
+      harmonize = FALSE,
+      geometry = "zone"
+    ),
+    error = TRUE
+  )
+  expect_snapshot(
+    read_map(
+      city = "São Paulo",
+      year = 1978,
+      harmonize = FALSE,
+      geometry = "zone"
+    ),
+    error = TRUE
+  )
+  expect_snapshot(
+    read_map(
+      city = "São Paulo",
+      year = 1977,
+      harmonize = TRUE,
+      geometry = "zone"
+    ),
+    error = TRUE
+  )
 })
