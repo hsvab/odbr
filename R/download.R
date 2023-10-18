3a2f7e79322870f43dc060192b484a2c911fa0cd
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2023-10-18 18:48:24 -0300

    Save downloaded files at a temporary directory

--- R/download.R
+++ R/download.R
@@ -1,20 +1,21 @@
 # The "download_piggyback" function requires as parameter the filename_to_download
 download_piggyback <- function(filename_to_download) {
-  # Defining a temporary file path and saving it in "temp"
-  temp <- fs::file_temp()
+  # Defining our temporary directory
+  temp_dest_dir <- tempdir(check = TRUE)
 
   # Creating the temporary folder effectively
-  fs::dir_create(temp)
+  fs::dir_create(path = temp_dest_dir, recurse = TRUE)
 
   # Creating path + filename and saving to "temporary_filename"
-  temporary_filename <- paste0(temp, "/", filename_to_download)
+  temp_full_file_path <- paste0(temp_dest_dir, "/", filename_to_download)
 
   # Uploading the file to a release of the odbr repo - release specified in the parameter
   piggyback::pb_download(
     file = filename_to_download,
     repo = "hsvab/odbr",
-    dest = temp
+    dest = temp_dest_dir
   )
+
   # The function returns the R object "temporary_filename"
-  return(temporary_filename)
+  return(temp_full_file_path)
 }
